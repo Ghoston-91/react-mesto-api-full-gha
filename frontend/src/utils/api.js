58d3612b1d9f423ad41ccba208808c2dc5b7e1a6
Vitaly Chikhalov <ghoston.91@yandex.ru>
2023-04-10 17:22:27 +0300

    test

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -33,100 +33,84 @@ class Api {
           .then(this._checkResponse)
     }
 
-    editUserProfile(newName, newAbout) {
-        return fetch(`${this.baseUrl}/users/me`, {
-          method: 'PATCH',
-          headers: {
-            'Authorization' : `Bearer ${localStorage.getItem('token')}`,
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            name: newName,
-            about: newAbout
-          })
-        })
-        .then(this._checkResponse)
-      }
+    editUserProfile(userProfile) {
+      return fetch(`${this._baseUrl}/users/me`, {
+          method: "PATCH",
+          headers: this._headers,
+          body: JSON.stringify(userProfile),
+      }).then((res) => {
+          return this._checkResponse(res);
+      });
+  }
 
-    addNewCard(cardData) {
+      addNewCard(cardData) {
         return fetch(`${this._baseUrl}/cards`, {
-            method: 'POST',
-            headers: {
-              'Authorization' : `Bearer ${localStorage.getItem('token')}`,
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              name: cardData.name,
-              link: cardData.link
-            })
-          })
-          .then(this._checkResponse)
-        }
+            method: "POST",
+            headers: this._headers,
+            body: JSON.stringify({ name: cardData.title, link: cardData.link }),
+        }).then((res) => {
+            return this._checkResponse(res);
+        });
+    }
 
     deleteCard(cardId) {
-        return fetch(`${this._baseUrl}/cards/${cardId}`, {
-            method: "DELETE",
-            headers: {
-                'Authorization' : `Bearer ${localStorage.getItem('token')}`,
-                'Content-Type': 'application/json'
-              },
-            })
-            .then(this._checkResponse)
-          }
+      return fetch(`${this._baseUrl}/cards/${cardId}`, {
+        method: "DELETE",
+        headers: this._headers,
+    }).then((res) => {
+        return this._checkResponse(res);
+    });
+}
 
     putLikeCard(cardId) {
         return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
             method: "PUT",
-            headers: {
-                'Authorization' : `Bearer ${localStorage.getItem('token')}`,
-                'Content-Type': 'application/json'
-              },
-            })
-            .then(this._checkResponse)
-          }
+            headers: this._headers,
+          }).then((res) => {
+              return this._checkResponse(res);
+          });
+      }
+      
 
     deleteLikeCard(cardId) {
         return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
             method: "DELETE",
-            headers: {
-                'Authorization' : `Bearer ${localStorage.getItem('token')}`,
-                'Content-Type': 'application/json'
-              },
-            })
-            .then(this._checkResponse)
-          }
+            headers: this._headers,
+          }).then((res) => {
+              return this._checkResponse(res);
+          });
+      }
+      
 
     changeAvatar(link) {
         return fetch(`${this._baseUrl}/users/me/avatar`, {
-            method: "PATCH",
-            headers: {
-                'Authorization' : `Bearer ${localStorage.getItem('token')}`,
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                avatar: link
-              })
-            })
-            .then(this._checkResponse)
-          }
+          method: "PATCH",
+          headers: this._headers,
+          body: JSON.stringify({
+              avatar: link,
+          }),
+      }).then((res) => {
+          return this._checkResponse(res);
+      });
+  }
 
     changeLikeCardStatus(cardId, isLiked) {
-        if (isLiked) {
-            return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-                method: "PUT",
-                headers: this._headers,
-            }).then((res) => {
-                return this._checkResponse(res);
-            });
-        } else {
-            return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-                method: "DELETE",
-                headers: this._headers,
-            }).then((res) => {
-                return this._checkResponse(res);
-            });
-        }
+      if (isLiked) {
+        return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+            method: "PUT",
+            headers: this._headers,
+        }).then((res) => {
+            return this._checkResponse(res);
+        });
+    } else {
+        return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+            method: "DELETE",
+            headers: this._headers,
+        }).then((res) => {
+            return this._checkResponse(res);
+        });
     }
+}
 }
 
 const apiConnect = new Api({
