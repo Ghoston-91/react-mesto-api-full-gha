66aa48b2292b167bfc422e770cabc954ce0e0191
Vitaly Chikhalov <ghoston.91@yandex.ru>
2023-04-10 15:38:09 +0300

    test

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,27 +1,22 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const NotAuthError = require('../utils/NotAuthError');
 const { NODE_ENV_DEV } = require('../utils/constants');
-
-const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new NotAuthError('Необходима авторизация'));
-    return;
   }
-
-  const token = authorization.replace('Bearer ', '');
   let payload;
+  const token = authorization.replace('Bearer ', '');
 
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : NODE_ENV_DEV);
   } catch (err) {
     next(new NotAuthError('Необходима авторизация'));
-    return;
   }
 
   req.user = payload;
-
   next();
 };
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,24 +1,26 @@
 const allowedCors = [
-  // 'https://ghoston91.nomoredomains.monster',
-  // 'http://ghoston91.nomoredomains.monster/',
+  'https://ghoston91.nomoredomains.monster',
+  'http://ghoston91.nomoredomains.monster/',
   'localhost:3000',
 ];
 
-function checkAccess(req, res, next) {
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { method } = req;
   const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Origin', '*');
   }
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-  return next();
-}
 
-module.exports = { checkAccess };
+  next();
+};
