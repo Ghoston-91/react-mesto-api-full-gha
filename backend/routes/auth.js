0f7b8d6e0406066c9359bc0d079591bf45c56b81
Vitaly Chikhalov <ghoston.91@yandex.ru>
2023-04-10 12:03:18 +0300

    test

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,14 +3,14 @@ const { Joi, celebrate } = require('celebrate');
 const { login, createUser } = require('../controllers/users');
 const patternValid = require('../utils/patternValid');
 
-router.post('/signin', celebrate({
+router.post('/sign-in', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
 
-router.post('/signup', celebrate({
+router.post('/sign-up', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
